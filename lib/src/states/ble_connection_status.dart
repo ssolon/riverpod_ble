30c2a71c2021250bc106384072f8716de7c03517
ssolon <ssolon@usa.net>
2023-08-16 17:55:39 -0700

    Remove deprecated connected states

--- lib/src/states/ble_connection_status.dart
+++ lib/src/states/ble_connection_status.dart
@@ -5,10 +5,8 @@ part 'ble_connection_status.freezed.dart';
 @freezed
 class BleConnectionStatus with _$BleConnectionStatus {
   factory BleConnectionStatus.initial() = Initial;
-  factory BleConnectionStatus.connecting() = Connecting;
   factory BleConnectionStatus.error(Object error, StackTrace? stackTrace) =
       Error;
   factory BleConnectionStatus.connected() = Connected;
-  factory BleConnectionStatus.disconnecting() = Disconnecting;
   factory BleConnectionStatus.disconnected() = Disconnected;
 }
