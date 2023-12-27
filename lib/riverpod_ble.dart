132812e94a48e54256839f16df677354be13164b
ssolon <ssolon@usa.net>
2023-12-26 23:31:10 -0800

    Export ble_scan_result.dart
    Was forgotten.

--- lib/riverpod_ble.dart
+++ lib/riverpod_ble.dart
@@ -1,6 +1,7 @@
 library riverpod_ble;
 
 export "src/ble.dart";
+export "src/states/ble_scan_result.dart";
 export "src/states/ble_device.dart";
 export "src/states/ble_service.dart";
 export "src/states/ble_uuid.dart";
